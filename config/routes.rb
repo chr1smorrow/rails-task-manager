2d5b213b50af0803bb120683846cf5ec8506c6f3
Chris Morrow <chrismorrow@blueyonder.co.uk>
2018-05-15 19:07:40 +0100

    Ex1 done

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,21 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+# read all
+get "tasks", to: "tasks#index", as: :tasks
+
+# create one
+get "tasks/new", to: "tasks#new", as: :new_task
+post "tasks", to: "tasks#create"
+
+# show
+get "tasks/:id", to: "tasks#show", as: :task
+
+# update one
+get "tasks/:id/edit", to: "tasks#edit", as: :edit_task
+patch "tasks/:id", to: "tasks#update"
+
+# delete one
+delete "tasks/:id", to: "tasks#destroy"
+
 end
